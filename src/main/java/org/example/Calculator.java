b1263d88a9e2a6d0f9f8cad16f12dbc384a5d1e3
plusmate <psejin1478@gmail.com>
2025-01-07 15:17:16 +0900

    계산 기록 로직 추가

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.LinkedList;
+
+public class Calculator {
+    LinkedList<Integer> resultList = new LinkedList<>();
+    int result = 0;
+
+    public int calculate(int num1, int num2, String operator) {
+        if (operator.equals("+")) {
+            result = num1 + num2;
+            resultList.add(result);
+        } else if (operator.equals("-")) {
+            result = num1 - num2;
+            resultList.add(result);
+        } else if (operator.equals("*")) {
+            result = num1 * num2;
+            resultList.add(result);
+        } else if (operator.equals("/")) {
+            result = num1 / num2;
+            resultList.add(result);
+        }
+
+        return result;
+    }
+
+    public void removeResult() {
+        if (!resultList.isEmpty()) {
+            Integer removeNum = resultList.remove();
+            System.out.println("기록이 삭제되었습니다.");
+        } else {
+            System.out.println("계산 기록이 없습니다.");
+        }
+    }
+
+    /**
+     * GETTER / SETTER
+     */
+    public void setResultList(LinkedList<Integer> resultList) {
+        this.resultList = resultList;
+    }
+
+    public LinkedList<Integer> getResultList() {
+        return resultList;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,16 @@
 package org.example;
 
 import java.util.InputMismatchException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator();
         int result = 0;
-        int operand_1;
-        int operand_2;
+        int num1;
+        int num2;
         String operator;
         String exitCalc = "";
 
@@ -16,9 +18,9 @@ public class Main {
             while (true) {
                 try {
                     System.out.print("첫 번째 숫자를 입력하세요: ");
-                    operand_1 = sc.nextInt();
+                    num1 = sc.nextInt();
 
-                    if (operand_1 < 0) {
+                    if (num1 < 0) {
                         System.out.println("양수만 입력 가능합니다. 다시 입력하세요.");
                         continue;
                     }
@@ -41,12 +43,12 @@ public class Main {
             while (true) {
                 try {
                     System.out.print("두 번째 숫자를 입력하세요: ");
-                    operand_2 = sc.nextInt();
+                    num2 = sc.nextInt();
 
-                    if (operand_2 < 0) {
+                    if (num2 < 0) {
                         System.out.println("양수만 입력 가능합니다. 다시 입력하세요.");
                         continue;
-                    } else if (operand_2 == 0) {
+                    } else if (num2 == 0) {
                         System.out.println(" 0으로는 나눌 수 없습니다. 다시 입력하세요.");
                         continue;
                     }
@@ -59,19 +61,36 @@ public class Main {
                 }
             }
 
-            if (operator.equals("+")) {
-                result = operand_1 + operand_2;
-            } else if (operator.equals("-")) {
-                result = operand_1 - operand_2;
-            } else if (operator.equals("*")) {
-                result = operand_1 * operand_2;
-            } else if (operator.equals("/")) {
-                result = operand_1 / operand_2;
-            }
+//            if (operator.equals("+")) {
+//                result = num1 + num2;
+//            } else if (operator.equals("-")) {
+//                result = num1 - num2;
+//            } else if (operator.equals("*")) {
+//                result = num1 * num2;
+//            } else if (operator.equals("/")) {
+//                result = num1 / num2;
+//            }
+            result = calculator.calculate(num1, num2, operator);
 
             System.out.println("결과: " + result +
-                    " ( " + operand_1 + " " + operator + " " + operand_2 + " = " + result + " ) ");
-            System.out.print("더 계산 시 아무거나 입력하세요. (exit 입력 시 종료) : ");
+                    " ( " + num1 + " " + operator + " " + num2 + " = " + result + " ) ");
+            System.out.println();
+
+            while (true){
+                System.out.println("계산 기록");
+                System.out.println("==============================");
+                System.out.println(calculator.getResultList());
+                System.out.println("계산 기록을 삭제 하시겠습니까?( 첫 기록부터 삭제 / 삭제시 Y 입력 ) : ");
+
+                String isDelete = sc.nextLine();
+                if (isDelete.equalsIgnoreCase("Y")) {
+                    calculator.removeResult();
+                }else {
+                    break;
+                }
+            }
+
+            System.out.println("더 계산 시 아무거나 입력하세요. (exit 입력 시 종료 / edit 입력 시 계산 기록 편집) : ");
             exitCalc = sc.nextLine();
         } while (!exitCalc.equals("exit"));
     }
